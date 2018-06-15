3f6757352c2cc08944a783107ab8245bc9a0f487
Steven Miyakawa <steven.miyakawa@here.com>
2018-06-14 21:18:35 -0700

    Update Conference component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,12 @@ import Speakers from '../components/Speakers'
 import AdditionalEvents from '../components/AdditionalEvents'
 import Sponsors from '../components/Sponsors'
 import Highlights from '../components/Highlights'
-import ConvinceYourBoss from '../components/ConvinceYourBoss'
 
 import BuyTickets from '../components/BuyTickets'
 
 const IndexPage = () => (
   <main style={{ textAlign: 'center', marginBottom: 100 }}>
     <Conference />
-    <hr />
     <Speakers />
     <hr />
     <AdditionalEvents />
