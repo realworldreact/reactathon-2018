572d0c77369d6bc8fcda5e3897d4c4c812f82fbd
Steven Miyakawa <steven.miyakawa@here.com>
2018-06-16 22:44:24 -0700

    Add new speakers and update styles

--- src/components/Speakers/Speaker/index.js
+++ src/components/Speakers/Speaker/index.js
@@ -1,17 +1,39 @@
 import styles from './speaker.module.css'
 import React from 'react'
+import xIcon from './icon-x.svg'
+import triangleIcon from './icon-triangle.svg'
+import squareIcon from './icon-square.svg'
 
-const Speaker = ({ name, company, twitter, github, imgSlug }) => {
+const Speaker = ({ name, company, topic, imgSlug }) => {
   return (
     <div className={styles.speaker}>
-      <div style={{ display: 'flex' }}>
+      {imgSlug ? (
         <img
-          src={require(`../images/${imgSlug}@3x.png`)}
+          src={require(`../images/${imgSlug}`)}
           alt={`${name}'s image`}
           className={styles['speaker-image']}
         />
-        <div style={{ marginLeft: 40 }}>
-          ksajdf
+      ) : (
+        <div className={styles.placeholder} />
+      )}
+      <div className={styles['info-container']}>
+        <div className={styles['info-row']}>
+          <div className={styles['icon-container']}>
+            <img src={xIcon} alt="x icon" />
+          </div>
+          <p>{name}</p>
+        </div>
+        <div className={styles['info-row']}>
+          <div className={styles['icon-container']}>
+            <img src={triangleIcon} alt="triangle icon" />
+          </div>
+          <p>{company}</p>
+        </div>
+        <div className={styles['info-row']}>
+          <div className={styles['icon-container']}>
+            <img src={squareIcon} alt="square icon" />
+          </div>
+          <p>{topic}</p>
         </div>
       </div>
     </div>
--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -4,21 +4,33 @@ import Speaker from './Speaker'
 import speakers from './speakers.json'
 
 const Speakers = () => (
-  <section className={styles.speakers} style={{ marginTop: 50 }}>
+  <section className={styles.speakers} style={{ marginTop: 60 }}>
     <main>
       <h1 style={{ padding: 80 }}>Speakers</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {speakers.map(({ name, company, twitter, github, imgSlug }, i) => (
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+      >
+        {speakers.map(({ name, company, topic, imgSlug }, i) => (
           <Speaker
-            key={`speaker-${i}`}
+            key={`${name}`}
             name={name}
             company={company}
-            twitter={twitter}
-            github={github}
+            topic={topic}
             imgSlug={imgSlug}
           />
         ))}
       </div>
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+      >
+        <Speaker
+          name="Brian Douglas"
+          company="Github"
+          topic="Master of Ceremonies"
+          imgSlug="speaker-brian-douglas.png"
+        />
+      </div>
+      <a href="#">full schedule of talks</a>
     </main>
   </section>
 )
