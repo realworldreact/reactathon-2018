e7721bc1b45821ab59788cdb322d59528816b743
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2018-06-23 12:58:48 -0700

    update speaker grid styles and copy

--- src/components/Conference/index.js
+++ src/components/Conference/index.js
@@ -8,7 +8,7 @@ const Conference = () => (
   >
     <h1 style={{ marginBottom: 30 }}>Your Neighborhood React Conference</h1>
     <p style={{ maxWidth: 610, marginLeft: 'auto', marginRight: 'auto' }}>
-      This special edition of Reactathon features an evening + full day of
+      This special edition of Reactathon features an evening reception + full day of
       technical talks in the heart of San Francisco on all things React. Come
       hang out with and learn from some of the brightest minds and best speakers
       in the JS/React community.
