0175210f1b1d4dfb8e77e81db6f9e77cb2bb9107
Steven Miyakawa <steven.miyakawa@here.com>
2018-06-23 21:13:57 -0700

    Change back to tabbed Buy Tickets section

--- src/components/BuyTickets/index.js
+++ src/components/BuyTickets/index.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react'
-import styles from './index.module.css'
-import { BUY_TICKETS_ID } from '../../constants'
+import classNames from 'classnames'
+import styles from './buyTickets.module.css'
+import createUniverseTicketWidget from '../UniverseTicketWidget/createUniverseTicketWidget'
+import { BUY_TICKETS_ID, ROUND_ONE_TICKET_WIDGET_ID } from '../../constants'
 
-const BuyTickets = () => (
-  <section
-    id={BUY_TICKETS_ID}
-    className={`container ${styles['component-container']}`}
-    style={{ marginTop: 50 }}
-  >
-    <h1>Buy Tickets</h1>
-    <div style={{ marginTop: 30 }}>
-      <p>Super Early Bird Tickets now on sale at Eventbrite</p>
-    </div>
-  </section>
+const RoundOneUniversteTicketWidget = createUniverseTicketWidget(
+  ROUND_ONE_TICKET_WIDGET_ID
 )
+
+class BuyTickets extends Component {
+  constructor() {
+    super()
+
+    this.state = { activeTab: 1 }
+  }
+
+  handleTabClick(e, tabNum) {
+    e.preventDefault()
+    this.setState({ activeTab: tabNum })
+  }
+
+  renderActiveTabContent(activeTab) {
+    if (activeTab === 1) {
+      return <RoundOneUniversteTicketWidget />
+    } else if (activeTab === 2) {
+      return <p>Round 2 tickets not on sale yet.</p>
+    } else if (activeTab === 3) {
+      return <p>Round 3 tickets not on sale yet.</p>
+    }
+  }
+
+  render() {
+    const { activeTab } = this.state
+
+    return (
+      <section
+        id={BUY_TICKETS_ID}
+        className={styles['buy-tickets']}
+        style={{ marginTop: 50 }}
+      >
+        <h1>Buy Tickets</h1>
+        <div style={{ margin: 'auto', marginTop: 20, marginBottom: 50 }}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {[1, 2, 3].map(tabNum => (
+              <div key={`round-${tabNum}`}>
+                <a
+                  href="#"
+                  className={classNames(styles.tab, {
+                    [styles.active]: activeTab === tabNum,
+                  })}
+                  onClick={e => this.handleTabClick(e, tabNum)}
+                >
+                  Round {tabNum}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        {this.renderActiveTabContent(activeTab)}
+      </section>
+    )
+  }
+}
 
 export default BuyTickets
