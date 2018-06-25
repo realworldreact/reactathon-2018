a98104aee2e95fe413549ace639c583311322f9a
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2018-06-24 20:37:23 -0700

    update conference universe ticket widget

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const BUY_TICKETS_ID = 'buy-tickets'
 
 // TODO: replace with new ticket ID
 export const ROUND_ONE_TICKET_WIDGET_ID =
-  'reactathon-fundamentals-conference-tickets-san-francisco-BTD5GW'
+  'reactathon-conference-tickets-san-francisco-K3WSVY'
