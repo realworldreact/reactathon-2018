be447d31d8491fc3075eee0553731ecaa4a24619
Steven Miyakawa <steven.miyakawa@here.com>
2018-08-26 18:38:29 -0700

    Fix React warnings

--- src/components/Conference/index.js
+++ src/components/Conference/index.js
@@ -24,10 +24,10 @@ const Conference = () => (
         marginTop: 30,
       }}
     >
-      <li style={{ display: 'inline', paddingRight: '20' }}>
+      <li style={{ display: 'inline', paddingRight: 20 }}>
         <Link to="/schedule">Conference Schedule</Link>
       </li>
-      <li style={{ display: 'inline', paddingLeft: '20' }}>
+      <li style={{ display: 'inline', paddingLeft: 20 }}>
         <Link to="/workshops">Workshops Schedule</Link>
       </li>
     </ul>
