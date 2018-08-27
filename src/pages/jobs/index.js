8790e009ac4690a434d65175c2bb11fb1a1ce382
Steven Miyakawa <steven.miyakawa@here.com>
2018-08-26 20:08:02 -0700

    Add key to each job listing

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -10,8 +10,8 @@ const JobsPage = () => (
       <h2 style={{ marginBottom: 50 }}>
         Please take the time to read our code of conduct.
       </h2>
-      {jobs.map(({ title, description, fullDescriptionURL }) => (
-        <div style={{ marginBottom: 30 }}>
+      {jobs.map(({ title, description, fullDescriptionURL }, i) => (
+        <div key={`job-${i}`} style={{ marginBottom: 30 }}>
           <h3 style={{ marginBottom: 15 }}>{title}</h3>
           <p style={{ marginBottom: 15 }}>{description}</p>
           <a href={fullDescriptionURL} target="_blank">
